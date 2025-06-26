a9dc0d5ec8254d8f06fb739e8875ff5b333aa067
gois murilo <murilo1997mendes@gmail.com>
2025-06-25 21:32:25 -0300

    Create ex1L7.py

---
+++ ex1L7.py
@@ -0,0 +1,16 @@
+x = []
+i = 0
+soma = 0
+while i<4:
+    nota = (int(input("Digite as notas :")))
+    x.append(nota)
+    print(x)
+    soma += nota
+    i = i+1
+
+media = soma / 4
+print(media)
+
+    
+
+
